4a4d0153adde17de9e2d2d1647ab8207b8c4404c
Franky55 <43887787+Franky55@users.noreply.github.com>
2024-11-05 15:24:17 -0500

    boring stuff, added new endPoints for Machine inventory

---
+++ snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/business/machine_surface.java
@@ -0,0 +1,7 @@
+package ca.usherbrooke.fgen.api.business;
+
+public class machine_surface {
+    public String emplacement_machine;
+    public String model_type_m;
+    public String manufacturier_type_m;
+}
