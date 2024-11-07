d60348de2f6ef04116c77e2720f9fc224cf0a70b
Franky55 <43887787+Franky55@users.noreply.github.com>
2024-11-07 10:01:45 -0500

    fuck jakarta

--- snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/service/authentificationService.java
+++ snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/service/authentificationService.java
@@ -7,8 +7,6 @@ import javax.inject.Inject;
 import io.quarkus.security.identity.SecurityIdentity;
 
 //import jakarta.inject.Inject;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
 
 @Path("/api")
 @Produces(MediaType.APPLICATION_JSON)
