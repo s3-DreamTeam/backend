9de2216cdd6a7bde67e8bb6b559dcfabeb2c0b18
Franky55 <43887787+Franky55@users.noreply.github.com>
2024-11-16 16:42:05 -0500

    fixed error //

--- snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/service/product_inventory_Service.java
+++ snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/service/product_inventory_Service.java
@@ -25,52 +25,52 @@ public class product_inventory_Service {
         return Mapper.getAllProduct(id_usager);
     }
 
-    @GET
-    @Path("ProductInventory/Get/Image")
-    public List<product_template_page> getCompactProductImage(@PathParam("id_usager") Integer id_usager)
-    {
-        return Mapper.getCompactProductTemplate(id_usager);
-    }
-
-    @GET
-    @Path("ProductInventory/Get/Surface")
-    public List<Integer> getCompactProductSurface(@PathParam("id_usager") Integer id_usager)
-    {
-        List<Integer> listInt = new ArrayList<>();
-
-        return listInt;
-    }
-
-    @GET
-    @Path("ProductInventory/Get/AllID")
-    public List<String> getCompactProductAllID()
-    {
-        List<String> listInt = new ArrayList<>();
-
-        return listInt;
-    }
-
-    @GET
-    @Path("ProductInventory/New")
-    public product_template_page setCompactProduct(@PathParam("id_usager") Integer id_usager)
-    {
-        product_template_page product = new product_template_page();
-        return product;
-    }
-
-    @GET
-    @Path("ProductInventory/Modify")
-    public product_template_page modifyCompactProduct(@PathParam("id_usager") Integer id_usager)
-    {
-        product_template_page product = new product_template_page();
-        return product;
-    }
-
-    @GET
-    @Path("ProductInventory/Delete")
-    public product_template_page deleteCompactProduct(@PathParam("id_usager") Integer id_usager)
-    {
-        product_template_page product = new product_template_page();
-        return product;
-    }
+//    @GET
+//    @Path("ProductInventory/Get/Image")
+//    public List<product_template_page> getCompactProductImage(@PathParam("id_usager") Integer id_usager)
+//    {
+//        return Mapper.getCompactProductTemplate(id_usager);
+//    }
+//
+//    @GET
+//    @Path("ProductInventory/Get/Surface")
+//    public List<Integer> getCompactProductSurface(@PathParam("id_usager") Integer id_usager)
+//    {
+//        List<Integer> listInt = new ArrayList<>();
+//
+//        return listInt;
+//    }
+//
+//    @GET
+//    @Path("ProductInventory/Get/AllID")
+//    public List<String> getCompactProductAllID()
+//    {
+//        List<String> listInt = new ArrayList<>();
+//
+//        return listInt;
+//    }
+//
+//    @GET
+//    @Path("ProductInventory/New")
+//    public product_template_page setCompactProduct(@PathParam("id_usager") Integer id_usager)
+//    {
+//        product_template_page product = new product_template_page();
+//        return product;
+//    }
+//
+//    @GET
+//    @Path("ProductInventory/Modify")
+//    public product_template_page modifyCompactProduct(@PathParam("id_usager") Integer id_usager)
+//    {
+//        product_template_page product = new product_template_page();
+//        return product;
+//    }
+//
+//    @GET
+//    @Path("ProductInventory/Delete")
+//    public product_template_page deleteCompactProduct(@PathParam("id_usager") Integer id_usager)
+//    {
+//        product_template_page product = new product_template_page();
+//        return product;
+//    }
 }
