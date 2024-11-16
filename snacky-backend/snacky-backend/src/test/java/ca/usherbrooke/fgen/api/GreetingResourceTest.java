3189d16bc962794a15dd618a38095691366b2332
vicsk8er <victor.larose@hotmail.com>
2024-11-16 14:51:23 -0500

    importation dépendance pour bypass l'autentification pour les tests

--- snacky-backend/snacky-backend/src/test/java/ca/usherbrooke/fgen/api/GreetingResourceTest.java
+++ snacky-backend/snacky-backend/src/test/java/ca/usherbrooke/fgen/api/GreetingResourceTest.java
@@ -1,31 +1,39 @@
-//package ca.usherbrooke.fgen.api;
-//
-//
-//import ca.usherbrooke.fgen.api.business.machine_template;
-//import ca.usherbrooke.fgen.api.business.testClass;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import io.quarkus.test.junit.QuarkusTest;
-//import org.junit.jupiter.api.Test;
-//
-//import javax.ws.rs.*;
-//import javax.ws.rs.core.MediaType;
-//import javax.ws.rs.core.Response;
-//import java.util.UUID;
-//
-//import static io.restassured.RestAssured.given;
-//import static org.hamcrest.CoreMatchers.is;
-//
-//@QuarkusTest
-//public class GreetingResourceTest {
-//
-//    //    @Test
-////    public void testGetMessagePara() {
-////        given()
-////                .when().get("/api/test")
-////                .then()
-////                .statusCode(200)
-////                .body(is("hello"));
-////    }
+package ca.usherbrooke.fgen.api;
+
+
+import ca.usherbrooke.fgen.api.business.machine_template;
+import ca.usherbrooke.fgen.api.business.testClass;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.security.identity.SecurityIdentity;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.security.TestSecurity;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+import javax.inject.Inject;
+@QuarkusTest
+public class GreetingResourceTest {
+    @Inject
+    SecurityIdentity identity;  // Injecter l'instance de SecurityIdentity
+    //    @Test
+//    public void testGetMessagePara() {
+//        given()
+//                .when().get("/api/test")
+//                .then()
+//                .statusCode(200)
+//                .body(is("hello"));
+//    }
 //    @Test
 //    public void testGetMessagePara() {
 //        // Création d'un objet machine_template avec des valeurs
@@ -64,76 +72,98 @@
 //                .statusCode(200) // Vérifie que le code de retour est 200
 //                .body(is(requestBody)); // Vérifie que la réponse JSON correspond à la requête JSON
 //    }
+//    @Test
+//    public void testInsertUsagerEndpoint() {
+//        given()
+//                .when().get("/api/insertUsager")
+//                .then()
+//                .statusCode(200) // Vérifie que le code HTTP est 200
+//                .body(is("0")); // Vérifie que la réponse contient "0"
+//    }
+//    @Test
+//    @TestSecurity(authorizationEnabled = false)
+//    public void testGetMachinesSpecificsAllID() {
+//        String userID = "mockUserID";
+//
+//        given()
+//                .header("Authorization", "Bearer mockToken")  // Token fictif
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .when()
+//                .get("/api/MachineTemplate/Get/AllID")
+//                .then()
+//                .statusCode(200)  // Assurez-vous que la réponse est OK
+//                .body("size()", equalTo(3)); // Vérifier la taille de la réponse
+//    }
+}
+
+
+//package ca.usherbrooke.fgen.api.service;
+//
+//import ca.usherbrooke.fgen.api.business.machine_template;
+//import ca.usherbrooke.fgen.api.business.testClass;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//
+//import javax.inject.Inject;
+//import javax.ws.rs.*;
+//        import javax.ws.rs.core.MediaType;
+//import javax.ws.rs.core.Response;
+//import java.util.List;
+//
+//@Path("/api")
+//@Produces(MediaType.APPLICATION_JSON)
+//@Consumes(MediaType.APPLICATION_JSON)
+//public class test {
+//
+//    @GET
+//    @Path("testGet")
+//    public String getMessage() {
+//
+////        testClass responseDTO = new testClass();
+////        responseDTO.message = "Message.exe ";
+////        return Response.ok(responseDTO).build();
+//        return "Ta mere";
+//    }
+//
+//    @POST
+//    @Path("testPost")
+//    public Response postMessage() {
+//
+//        testClass responseDTO = new testClass();
+//        responseDTO.message = "Message.exe ";
+//        return Response.ok(responseDTO).build();
+//        //return responseDTO;
+//    }
+//
+//    @POST
+//    @Path("test")
+//    public String getMessagePara(machine_template machineTemplate) {
+//
+//        //return jsonString;
+////        testClass responseDTO = new testClass();
+////        responseDTO.message = "Message.exe ";
+////        return Response.ok(responseDTO).build();
+//        //ObjectMapper objectMapper = new ObjectMapper();
+//
+//        try {
+//            //machine_template machineTemplate = objectMapper.readValue(jsonString, machine_template.class);
+//            //Mapper.createMachineTemplate(new authentificationService.User(identity).getUserID(), machineTemplate);
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            String jsonString = objectMapper.writeValueAsString(machineTemplate);
+//            return jsonString;
+//        } catch (Exception e) {
+//
+//            return "erreur:\n" + e.getMessage();
+//        }
+//    }
+//
+//    @POST
+//    @Path("testPostPara")
+//    public Response postMessagePara(String val) {
+//
+//        testClass responseDTO = new testClass();
+//        responseDTO.message = "Message.exe ";
+//        return Response.ok(responseDTO).build();
+//        //return responseDTO;
+//    }
 //}
-//
-//
-////package ca.usherbrooke.fgen.api.service;
-////
-////import ca.usherbrooke.fgen.api.business.machine_template;
-////import ca.usherbrooke.fgen.api.business.testClass;
-////import com.fasterxml.jackson.databind.ObjectMapper;
-////
-////import javax.inject.Inject;
-////import javax.ws.rs.*;
-////        import javax.ws.rs.core.MediaType;
-////import javax.ws.rs.core.Response;
-////import java.util.List;
-////
-////@Path("/api")
-////@Produces(MediaType.APPLICATION_JSON)
-////@Consumes(MediaType.APPLICATION_JSON)
-////public class test {
-////
-////    @GET
-////    @Path("testGet")
-////    public String getMessage() {
-////
-//////        testClass responseDTO = new testClass();
-//////        responseDTO.message = "Message.exe ";
-//////        return Response.ok(responseDTO).build();
-////        return "Ta mere";
-////    }
-////
-////    @POST
-////    @Path("testPost")
-////    public Response postMessage() {
-////
-////        testClass responseDTO = new testClass();
-////        responseDTO.message = "Message.exe ";
-////        return Response.ok(responseDTO).build();
-////        //return responseDTO;
-////    }
-////
-////    @POST
-////    @Path("test")
-////    public String getMessagePara(machine_template machineTemplate) {
-////
-////        //return jsonString;
-//////        testClass responseDTO = new testClass();
-//////        responseDTO.message = "Message.exe ";
-//////        return Response.ok(responseDTO).build();
-////        //ObjectMapper objectMapper = new ObjectMapper();
-////
-////        try {
-////            //machine_template machineTemplate = objectMapper.readValue(jsonString, machine_template.class);
-////            //Mapper.createMachineTemplate(new authentificationService.User(identity).getUserID(), machineTemplate);
-////            ObjectMapper objectMapper = new ObjectMapper();
-////            String jsonString = objectMapper.writeValueAsString(machineTemplate);
-////            return jsonString;
-////        } catch (Exception e) {
-////
-////            return "erreur:\n" + e.getMessage();
-////        }
-////    }
-////
-////    @POST
-////    @Path("testPostPara")
-////    public Response postMessagePara(String val) {
-////
-////        testClass responseDTO = new testClass();
-////        responseDTO.message = "Message.exe ";
-////        return Response.ok(responseDTO).build();
-////        //return responseDTO;
-////    }
-////}
-//
+
