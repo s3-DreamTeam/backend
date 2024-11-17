6dda55f5e46c2ba278212b1541cc18803be60d73
Franky55 <43887787+Franky55@users.noreply.github.com>
2024-11-17 11:50:06 -0500

    update pls

--- snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/service/test.java
+++ snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/service/test.java
@@ -53,7 +53,7 @@ public class test {
             return jsonString;
         } catch (Exception e) {
 
-            return "erreur du message:\n" + e.getMessage();
+            return "uptade tbk erreur du message:\n" + e.getMessage();
         }
     }
 
