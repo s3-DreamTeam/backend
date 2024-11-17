6d542c3ac082fa03235578ac427e9d18b8b72d44
Franky55 <43887787+Franky55@users.noreply.github.com>
2024-11-17 14:36:32 -0500

    on essaie de simplifier le Json

--- snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/business/machine_template_surface.java
+++ snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/business/machine_template_surface.java
@@ -8,14 +8,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class machine_template_surface {
 
     @JsonProperty("Manufacturer")
-    @JsonAlias({"Manufacturer"})
     public String manufacturier_type_m;
 
     @JsonProperty("Model")
-    @JsonAlias({"Model"})
     public String model_type_m;
 
     @JsonProperty("id")
-    @JsonAlias({"id"})
     public Integer id_machine;
 }
