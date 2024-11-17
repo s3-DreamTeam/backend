12d33496b49ba5e8c98f2cfeed876f34b4f07cfb
Franky55 <43887787+Franky55@users.noreply.github.com>
2024-11-17 11:58:42 -0500

    alias name changed

--- snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/business/machine_template_surface.java
+++ snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/business/machine_template_surface.java
@@ -6,11 +6,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class machine_template_surface {
 
     @JsonProperty("Manufacturer")
-    @JsonAlias({"manufacturer_machine"})
+    @JsonAlias({"manufacturier_type_m"})
     public String manufacturer_machine;
 
     @JsonProperty("Model")
-    @JsonAlias({"model_machine"})
+    @JsonAlias({"model_type_m"})
     public String model_machine;
 
     @JsonProperty("id")
