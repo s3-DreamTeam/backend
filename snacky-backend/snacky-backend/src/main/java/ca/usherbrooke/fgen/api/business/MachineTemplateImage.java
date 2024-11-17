0f9498166f011753af4cf41d472288678d450848
Franky55 <43887787+Franky55@users.noreply.github.com>
2024-11-17 15:46:59 -0500

    changed shawns probleme

--- snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/business/MachineTemplateImage.java
+++ snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/business/MachineTemplateImage.java
@@ -2,7 +2,7 @@ package ca.usherbrooke.fgen.api.business;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class MakeImage {
+public class MachineTemplateImage {
     @JsonProperty("Machine's Image")
     public String image;
 }
