ee6197d02fe8f5992f616d3ad6785b6286826cba
Franky55 <43887787+Franky55@users.noreply.github.com>
2024-11-17 15:36:40 -0500

    @JsonProperty("Machine's Image")

--- snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/business/MakeImage.java
+++ snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/business/MakeImage.java
@@ -1,5 +1,8 @@
 package ca.usherbrooke.fgen.api.business;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class MakeImage {
+    @JsonProperty("Machine's Image")
     public String image;
 }
