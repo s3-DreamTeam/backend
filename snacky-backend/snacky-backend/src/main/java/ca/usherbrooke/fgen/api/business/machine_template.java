7cb6a6bad26f0f79482f290639c1d0ba78d0a3ec
Franky55 <43887787+Franky55@users.noreply.github.com>
2024-11-16 23:55:00 -0500

    true this

--- snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/business/machine_template.java
+++ snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/business/machine_template.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@JsonIgnoreProperties
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class machine_template {
     public String id_usager;
 
