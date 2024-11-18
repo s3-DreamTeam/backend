141a3dc2c91681517b5c834d9b2142d6b83123d0
Franky55 <43887787+Franky55@users.noreply.github.com>
2024-11-18 14:41:18 -0500

    Update machine_template_Service.java

--- snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/service/machine_template_Service.java
+++ snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/service/machine_template_Service.java
@@ -24,32 +24,32 @@ public class machine_template_Service {
 
     @POST
     @Path("MachineTemplate/Get/Full")
-    public machine_template getTemplateMachine(machine_template machineTemplate) throws Exception {
+    public machine_template getTemplateMachine(Integer ID)
+    {
+        System.out.println("MachineTemplate/Get/Full\nRaw data received (ID):");
+        System.out.println(ID);
+
+        machine_template val = new machine_template();
+
         try
         {
-            ObjectMapper objectMapper = new ObjectMapper();
-            System.out.println("MachineTemplate/Get/Full\nRaw data received (machine_template):");
-            System.out.println(objectMapper.writeValueAsString(machineTemplate));
-
-            machine_template newMachineTemplate = new machine_template();
-
             information info = new information();
-            info.id_template = 3;
+            info.id_template = ID;
             info.id_usager = new authentificationService.User(identity).getUserID();
 
-            newMachineTemplate = Mapper.getTemplateMachine(info);
-
-            String jsonString = objectMapper.writeValueAsString(newMachineTemplate);
+            val = Mapper.getTemplateMachine(info);
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonString = objectMapper.writeValueAsString(val);
 
             System.out.println("MachineTemplate/Get/Full\nData received from DB:");
             System.out.println(jsonString);
 
-            return newMachineTemplate;
-
         } catch (Exception e) {
             System.out.println("failed to get or convert data from DB:");
-            throw new Exception("This is a general exception");
         }
+
+        return val;
+
     }
 
     @POST
