99a38944e8aa571a070cb62b46aa2ccb2b2a2a73
Franky55 <43887787+Franky55@users.noreply.github.com>
2024-11-18 15:33:36 -0500

    id

--- snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/business/machine_template.java
+++ snacky-backend/snacky-backend/src/main/java/ca/usherbrooke/fgen/api/business/machine_template.java
@@ -9,7 +9,7 @@ import io.quarkus.runtime.annotations.IgnoreProperty;
 public class machine_template {
     public String id_usager;
 
-    @JsonProperty("TemplateID")
+    @JsonProperty("id")
     public Integer id_type_m;
 
     @JsonProperty("Machine's Image")
